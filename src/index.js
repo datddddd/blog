ff93200ef27b2b6a06732cef9726b8396a3bcc5e
nguyen thanh dat <nguyendat5ka1@gmail.com>
2026-03-19 10:51:28 +0700

    REfactor code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const morgan = require('morgan')
-const { engine } = require('express-handlebars')
-const path = require('path')
+const express = require('express');
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
+const route = require('./routes');
+
+
 
 app.use(express.static(path.join(__dirname,'public')))
 
@@ -16,13 +19,10 @@ app.set('view engine', 'hbs')
 
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home', { title: 'My Blog' })
-})
 
-app.get('/news', (req, res) => {
-  res.render('new', { title: 'News' })
-})
+route(app);
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
